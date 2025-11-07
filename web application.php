b1852e852f2a01ffa15da6e6a0e849d14717ee53
vaishnav700 <vaishnavb0425@gmail.com>
2025-11-07 14:37:32 +0530

    Add files via upload

---
+++ web application.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>My Web Application</title>
+    </head>
+    <body>
+        <form action="web.php" method="POST">
+            <label>Enter Your name:</label>
+            <input type="text" name="name">
+            <input type="submit" value="Submit">
+        </form>
+        </body>
+</html>
